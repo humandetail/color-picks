a04d0eff46d22a5150ab83f04ad4e0ef5acca109
banlify <banlify@163.com>
2022-12-08 11:07:56 +0800

    chore(dep): add eslint plugin

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+
+  extends: [
+    '@vue/eslint-config-standard-with-typescript',
+    'plugin:vue/vue3-essential'
+  ],
+
+  ignorePatterns: [
+    'index.html',
+    'node_modules',
+    'dist'
+  ],
+
+  rules: {
+    'vue/multi-word-component-names': 0,
+    '@typescript-eslint/no-floating-promises': 0,
+    '@typescript-eslint/no-non-null-assertion': 0,
+    '@typescript-eslint/triple-slash-reference': 0,
+    '@typescript-eslint/consistent-type-assertions': 0,
+    '@typescript-eslint/strict-boolean-expressions': 0
+  }
+}
