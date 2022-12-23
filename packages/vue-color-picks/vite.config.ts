d27db1a456a31ac094c6985f29efec382b5c37f7
banlify <banlify@163.com>
2022-12-23 16:45:14 +0800

    build: 修改打包配置

--- packages/vue-color-picks/vite.config.ts
+++ packages/vue-color-picks/vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue(), dts()],
+  build: {
+    lib: {
+      entry: './src/VueColorPicks.vue',
+      name: '[name]',
+      formats: ['cjs', 'es']
+    },
+    rollupOptions: {
+      external: ['vue']
+    }
+  }
 })
