1ceaa0c25e88687d92b585bd08f822c2b8452dc3
laihanbin <512412344@qq.com>
2022-12-23 09:48:50 +0800

    feat: add example

---
+++ packages/example/src/main.ts
@@ -0,0 +1,22 @@
+import ColorPicks from 'color-picks'
+import './style.css'
+
+import 'color-picks/index.css'
+
+const bgColorPicks = new ColorPicks('.btn-bg')
+
+bgColorPicks.on('confirm', color => {
+  const oTarget = document.querySelector<HTMLDivElement>('.target')
+  if (oTarget) {
+    oTarget.style.backgroundColor = color
+  }
+})
+
+const fontColorPicks = new ColorPicks('.btn-font')
+
+fontColorPicks.on('confirm', color => {
+  const oTarget = document.querySelector<HTMLDivElement>('.target')
+  if (oTarget) {
+    oTarget.style.color = color
+  }
+})
